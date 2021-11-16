d21da39101ca8f9d7fb7f2a50fc1842a41d696d8
chandler-r <chandler.reyes123@gmail.com>
2021-11-16 08:53:41 -0500

    Added moves to move.js

--- js/moves.js
+++ js/moves.js
@@ -1,30 +1,76 @@
 export const Moves = {
-    behemothblade: {
-		num: 781,
+	quickattack: {
+		num: 98,
 		accuracy: 100,
-		basePower: 100,
+		basePower: 40,
 		category: "Physical",
-		name: "Behemoth Blade",
+		name: "Quick Attack",
+		pp: 30,
+		priority: 1,
+		flags: {contact: 1, protect: 1, mirror: 1},
+		secondary: null,
+		target: "normal",
+		type: "Normal",
+		contestType: "Cool",
+	},
+	closecombat: {
+		num: 370,
+		accuracy: 100,
+		basePower: 120,
+		category: "Physical",
+		name: "Close Combat",
 		pp: 5,
 		priority: 0,
 		flags: {contact: 1, protect: 1, mirror: 1},
+		self: {
+			boosts: {
+				def: -1,
+				spd: -1,
+			},
+		},
 		secondary: null,
 		target: "normal",
-		type: "Steel",
-	},
-    wildcharge: {
-		num: 528,
-		accuracy: 100,
-		basePower: 90,
-		category: "Physical",
-		name: "Wild Charge",
-		pp: 15,
-		priority: 0,
-		flags: {contact: 1, protect: 1, mirror: 1},
-		recoil: [1, 4],
-		secondary: null,
-		target: "normal",
-		type: "Electric",
+		type: "Fighting",
 		contestType: "Tough",
 	},
+	leer: {
+		num: 43,
+		accuracy: 100,
+		basePower: 0,
+		category: "Status",
+		name: "Leer",
+		pp: 30,
+		priority: 0,
+		flags: {protect: 1, reflectable: 1, mirror: 1},
+		boosts: {
+			def: -1,
+		},
+		secondary: null,
+		target: "allAdjacentFoes",
+		type: "Normal",
+		zMove: {boost: {atk: 1}},
+		contestType: "Cool",
+	},
+	payback: {
+		num: 371,
+		accuracy: 100,
+		basePower: 50,
+		basePowerCallback(pokemon, target, move) {
+			if (target.newlySwitched || this.queue.willMove(target)) {
+				this.debug('Payback NOT boosted');
+				return move.basePower;
+			}
+			this.debug('Payback damage boost');
+			return move.basePower * 2;
+		},
+		category: "Physical",
+		name: "Payback",
+		pp: 10,
+		priority: 0,
+		flags: {contact: 1, protect: 1, mirror: 1},
+		secondary: null,
+		target: "normal",
+		type: "Dark",
+		contestType: "Tough",
+	}
 }
