d9b727b09d566a58be227ebd5539710910dcc060
Justin Burrell <justinburrell715@gmail.com>
2021-11-17 09:13:00 -0500

    11.17.21 commit

--- python/battle_simulator.py
+++ python/battle_simulator.py
@@ -1,17 +1,20 @@
 import json
+from python import Pokemon
 
-with open('pokedex.json') as f:
+with open('../json/pokedex.json') as f:
    pokedex = json.load(f)
-
-pokemon1 = pokedex["venusaur"]
-pokemon2 = pokedex["aegislash"]
 
 def interface():
    input("Welcome to the Pokémon Battle Simulator. Press enter to continue")
+   input("Here is the battle situation " + "")
 
 print(interface())
 
+'''
+p1 = pokemon()
+p2 = pokemon()
 
-
-
-
+choose a move 
+choose m1
+p2.takeDamage(m1)
+'''
---
+++ python/pokemon.py
@@ -0,0 +1,29 @@
+class Pokemon:
+   def __init__(self):
+      self.pokemon_name = ""
+      self.pokemon_hp = 0
+      self.pokemon_atk = 0
+      self.pokemon_def = 0
+      self.pokemon_spa = 0
+      self.pokemon_spd = 0
+      self.pokemon_spe = 0
+      self.moves = []
+
+    #move function
+    def move(self):
+        # moves dictionary is appended so a Pokemon has four moves
+        # user selects a move 
+        # returns this move
+
+    #take_damage function
+    def take_damage(self, move):
+    
+    #switch function
+    def switch(self):
+    
+    #game_over function
+    def game_over(self):
+
+    #heal function
+
+    
