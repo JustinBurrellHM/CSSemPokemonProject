c23c2a2ce04d0801b60083537eee8158d2f30919
Justin Burrell <justinburrell715@gmail.com>
2021-11-30 11:03:49 -0500

    11-30-2021 Work Period

--- python/battle_simulator.py
+++ python/battle_simulator.py
@@ -9,19 +9,22 @@ moves = None
 with open('../json/moves.json') as f:
    moves = json.load(f)
 
-p1 = Pokemon(pokedex["zaciancrowned"], [moves["quickattack"]])
-p2 = Pokemon(pokedex["zaciancrowned"], [moves["quickattack"]])
+player1p1 = Pokemon(pokedex["zaciancrowned"], [moves["quickattack"]], [moves["closecombat"]])
+player1p2 = Pokemon(pokedex["venusaur"], [moves["quickattack"]], [moves["closecombat"]])
+
+player2p1 = Pokemon(pokedex["zaciancrowned"], [moves["closecombat"]])
+
+#automate pokemon_team list so the list can have every pokemon no the team 
+pokemon_team = [player1p1.name, player1p2.name]
 
 def interface():
    input("Welcome to the Pokémon Battle Simulator. Press enter to continue")
-   print(p1.name)
-   # input("Here is the battle situation " + p1 + "versus" + p2)
+   selection = input("Here is the battle situation: " + player1p1.name + " versus " + player2p1.name + ".\n" + "Your " + player1p1.name + " has " + str(player1p1.hp) + " HP. The opponent's " + player2p1.name + " has " + str(player2p1.hp) + " HP." + "\n" + "Would you like to [A]ttack or [S]witch Pokémon? ")
+
+   if selection == "A":
+      move_selection = input("Pick your move:\n" + str(player1p1.moves))
+   elif selection == "S":
+      switch_selection = input("Here is you team:\n" + str(pokemon_team) + "\n" + "")
 
 
 print(interface())
-
-
-
-# choose a move 
-# choose m1
-# p2.takeDamage(m1)
--- python/pokemon.py
+++ python/pokemon.py
@@ -1,33 +1,33 @@
 class Pokemon:
   def __str__(self):
-   return f"Name: {self.name}, HP: {self.pokemon_hp}, MOVES: {self.moves}"
+   return f"Name: {self.name}"
+  #  return f"Name: {self.name}, HP: {self.pokemon_hp}, MOVES: {self.moves}"
   
   def __init__(self, stats, moves):
     self.name = stats["name"]
-    self.pokemon_hp = stats["baseStats"]["hp"]
-    self.pokemon_atk = stats["baseStats"]["atk"]
-    self.pokemon_def = stats["baseStats"]["def"]
-    self.pokemon_spa = stats["baseStats"]["spa"]
-    self.pokemon_spd = stats["baseStats"]["spd"]
-    self.pokemon_spe = stats["baseStats"]["spe"]
-    self.moves = moves
+    self.hp = stats["baseStats"]["hp"]
+    self.atk = stats["baseStats"]["atk"]
+    self.defe = stats["baseStats"]["def"]
+    self.spa = stats["baseStats"]["spa"]
+    self.spd = stats["baseStats"]["spd"]
+    self.spe = stats["baseStats"]["spe"]
+    self.moves = [moves]
   
+  # move function
+  # def move(self, move):
+      # user selects a move
+      # print(self)
+      # returns this move
 
-    #move function
-    # def move(self):
-    #     # user selects a move 
-    #     # print(self)
-    #     # returns this move
+  # #take_damage function
+  # def take_damage(self, move):
+  
+  # #switch function
+  # def switch(self):
+  
+  # #game_over function
+  # def game_over(self):
 
-    # #take_damage function
-    # def take_damage(self, move):
-    
-    # #switch function
-    # def switch(self):
-    
-    # #game_over function
-    # def game_over(self):
-
-    # #heal function
+  # #heal function
 
     
