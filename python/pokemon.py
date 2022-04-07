5332295d59da2ec6442a27f8b49779348a956022
chandler-r <chandler.reyes123@gmail.com>
2022-04-07 08:43:57 -0400

    added crits, STAB, type effect, prio

--- python/pokemon.py
+++ python/pokemon.py
@@ -1,3 +1,4 @@
+import json
 from random import randint
 
 
@@ -10,6 +11,8 @@ class Moves:
     self.accuracy = moves["accuracy"]
     self.basepower = moves["basePower"]
     self.pp = moves["pp"]
+    self.type = moves["type"]
+    self.priority = moves["priority"]
 
 class Pokemon:
   def __repr__(self):
@@ -23,8 +26,12 @@ class Pokemon:
     self.spa = stats["baseStats"]["spa"]
     self.spd = stats["baseStats"]["spd"]
     self.spe = stats["baseStats"]["spe"]
-    self.type = [stats["types"]]
+    self.type = [stats["types"]] 
+    self.types = None
     self.moves = moves
+
+    with open('/Users/justinburrell/Desktop/HM Comp Sci/Comp Sci Sem/Sem1/Semester Project/CSSemPokemonProject/json/types.json') as f:
+      self.types = json.load(f)
   
   # move function
   def move(self, move, p2):
@@ -45,8 +52,20 @@ class Pokemon:
     y = randint(85,101)
     random = y/100
 
+    #type effectiveness
+    for i in p2.type:
+      if move.type == p2.type[i]:
+        type_effect = int(self.type[p2.type[i]]["damageTaken"][move.type])
+        if type_effect == 0:
+          type_effect = 1
 
-    damage = (((move.basepower*(self.atk/p2.defe))/50)+2)*crit*random
+    #same type attack bonus
+    for i in self.type:
+      if self.type[i] == move.type:
+        stab = 1.5
+
+
+    damage = (((move.basepower*(self.atk/p2.defe))/50)+2)*crit*random*stab*type_effect
     return damage
 
   # #heal function
